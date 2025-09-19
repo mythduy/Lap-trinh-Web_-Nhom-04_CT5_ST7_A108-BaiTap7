b1823405ca6f4e2c21e1688808b5f6d869d9020b
Khanh Duy <duongkhanhduycs@gmail.com>
2025-09-19 21:34:34 +0700

    Update

--- src/main/java/repository/CategoryRepository.java
+++ src/main/java/repository/CategoryRepository.java
@@ -1,5 +1,21 @@
 package repository;
 
-public class CategoryRepository {
+import entity.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+	List<Category> findByCategoryNameContaining(String name);
+
+	Page<Category> findByCategoryNameContaining(String name, Pageable pageable);
+	
+	List<Category> findByCategoryNameContainingIgnoreCase(String name);
 
 }
